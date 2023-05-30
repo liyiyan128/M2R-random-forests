bd720df1360295a4431594e8c81b6f71ea0a4d18
liyiyan128 <liyiyan128@gmail.com>
2023-05-30 11:09:46 +0100

    Decision_trees.py

---
+++ random-forests/decision_trees.py
@@ -0,0 +1,31 @@
+class Node:
+
+    def __init__(self, data, feature) -> None:
+        pass
+
+
+class DecisionTree:
+
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def _grow(self, X, y, depth):
+        # When to stop?
+        pass
+
+    def _information_gain(self):
+        pass
+
+
+def classification_error_rate(y):
+    pass
+
+
+def gini_index(y):
+    pass
