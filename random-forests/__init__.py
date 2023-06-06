fd502b9bf0096ada1e83e047fd128e358648a14c
Linden Graves <linden.graves21@imperial.ac.uk>
2023-06-06 16:07:19 +0100

    flake8

--- random-forests/__init__.py
+++ random-forests/__init__.py
@@ -1,2 +1,2 @@
-from .random_forests import RandomForest
-from .decision_trees import DecisionTree
+from .random_forests import RandomForest  # noqa F401
+from .decision_trees import DecisionTree  # noqa F401
