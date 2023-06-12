ac1fe656f98687e9c88e646152c90c58f4c204fe
liyiyan128 <liyiyan128@gmail.com>
2023-06-12 14:56:05 +0100

    Updated tests

--- tests/test_improved.py
+++ tests/test_improved.py
@@ -23,14 +23,14 @@ W_train, W_test, z_train, z_test = model_selection.train_test_split(W, y)
 
 
 # Test on continuous data.
-decision_tree = rf.DecisionTree()
+decision_tree = rf.ImprovedDecisionTree()
 decision_tree.fit(X_train, y_train)
 decision_tree._grow(X_train, y_train)
 y_predicted = decision_tree.predict(X_test)
 
 
 # Test on categorical data.
-decision_tree2 = rf.DecisionTree()
+decision_tree2 = rf.ImprovedDecisionTree()
 decision_tree2.fit(W_train, z_train, feature_type=[0, 0, 0, 1])
 decision_tree2._grow(W_train, z_train)
 z_predicted = decision_tree2.predict(W_test)
