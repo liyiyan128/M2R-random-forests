d3f5d8144edd00328b7fc00a721db5d2052ca42b
Linden Graves <linden.graves21@imperial.ac.uk>
2023-06-14 11:09:27 +0100

    bad random forests

--- random-forests/__init__.py
+++ random-forests/__init__.py
@@ -2,3 +2,4 @@ from .random_forests import RandomForest
 from .decision_trees import DecisionTree
 from .decision_trees_improved import ImprovedDecisionTree
 from .random_forests_improved import ImprovedRandomForest
+from .random_forests_bad import BadRandomForest
